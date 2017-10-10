f6c8b6d1aeb95e4e59051662a9cc0f25a64aae66
Thibault Helle <thibaulthelle@me.com>
2017-10-10 14:45:55 +0200

    func

--- scripts/arm_control.py
+++ scripts/arm_control.py
@@ -5,18 +5,35 @@ import rospy
 from geometry_msgs.msg import Point
 from std_msgs.msg import String
 from uarm.srv import *
-
+import math
 eef_x=float(0) # end effector position
 eef_y=float(0)
 eef_z=float(0)
 
 # zero position for the joints
-#j0_zero=float(38) # for NUC 16
-#j1_zero=float(130)
-#j2_zero=float(-7)
-j0_zero=float(28) # NUC 26
-j1_zero=float(95)
-j2_zero=float(-4)
+j0_zero=float(38)
+j1_zero=float(130)
+j2_zero=float(-7)
+
+LOWER_LIMITS = [-45, -3.0, -24.0, -65.0]
+UPPER_LIMITS = [115.0, 130.0, 90.0, 40.0]
+J2_J1_MIN_LIMIT_OFFSET= 18.5
+J2_J1_MAX_LIMIT_OFFSET = 153.0
+NUM_JOINTS = 4
+JOINT_NAMES = ['j0', 'j1', 'j2', 'j3']
+CALIBRATION_CONFIG = [45.0, 130.0, -4.83, 0.0]
+DEFAULT_EXECUTION_SLEEP_TIME = 0.1
+MAXIMAL_NUM_INTERPOLATION_STEPS = 80.0
+NO_INTERPOLATION = 'None'
+LINEAR_INTERPOLATION = 'Linear'
+CUBIC_INTERPOLATION = 'Cubic'
+# LINKS
+L1 = 10.645
+L2 = 2.117
+L3 = 14.825
+L4 = 16.02
+L5 = 3.3
+L6 = 5.9
 
 # reset arm to neutral position
 def resetPosition():
@@ -105,6 +122,14 @@ def controlCallback(data):
 def targetCallback(data):
     rospy.loginfo("Target position: x: %f, y: %f, z: %f", data.x, data.y, data.z)
 
+def inverse_kinematics(x, y, z, check_limits=True):
+    theta0=(math.atan2(y,x)* 180/ math.pi) + LOWER_LIMITS[0] # j0
+    theta1=0 * 180/ math.pi # j1
+    theta2= math.pi - math.acos((L1*L1 + L2*L2 - x*x -y*y)/(2*L1*L1 *L2*L2)) # j2
+    theta2=(theta2* 180/ math.pi) + LOWER_LIMITS[2]
+
+    return theta0,theta1,theta2
+
 if __name__ == "__main__":
     # Change the x, y, z coordinates for the arm
     # This is where we can subscribe to the coordinates from
@@ -113,10 +138,11 @@ if __name__ == "__main__":
     # use 1 second for large movements
 
     resetPosition() # reset arm
-    pumpClient(bool(False)) # disable pump
+    #pumpClient(bool(False)) # disable pump
 
     rospy.init_node('arm_node', anonymous=True)
     rospy.Subscriber("uarm/target_position", Point, targetCallback) # get target position
     rospy.Subscriber("uarm/control", String, controlCallback) # grab, reset etc.
+    print inverse_kinematics(10, 5, 0)
     rospy.spin()
 
